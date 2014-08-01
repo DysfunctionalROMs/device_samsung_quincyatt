9fc5c9c5721350f582827ef1a11796ebb8683a5a
Randy DuCharme <radio.ad5gb@gmail.com>
2014-07-31 20:25:52 -0500

    Update BoardConfig.mk
    Use custom kernel toolchain

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ TARGET_OTA_ASSERT_DEVICE := SGH-I717,quincyatt
 # Kernel
 TARGET_KERNEL_CONFIG        := cyanogenmod_quincyatt_defconfig
 TARGET_KERNEL_SOURCE        := kernel/samsung/msm8660-common
-#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7-sm
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-arm-eabi-4.7
 
 # Assert minimum baseband version
 TARGET_BOARD_INFO_FILE ?= device/samsung/quincyatt/board-info.txt
