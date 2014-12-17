91766a6b6b22e47bd94af8828ac54b991cb90630
Randy DuCharme <radio.ad5gb@gmail.com>
2014-12-17 15:19:52 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,8 @@ TARGET_KERNEL_CONFIG        := cyanogenmod_quincyatt_defconfig
 TARGET_KERNEL_SOURCE        := kernel/samsung/msm8660-common
 ##TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-arm-eabi-4.7
 #TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7-12.10/bin/arm-linux-gnueabihf-
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7-sm/bin/arm-eabi-
+##TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7-sm/bin/arm-eabi-
+TARGET_GCC_VERSION_ARM := 4.7-sm
 TARGET_CPU_VARIANT					:= cortex-a8
 
 # Assert minimum baseband version
