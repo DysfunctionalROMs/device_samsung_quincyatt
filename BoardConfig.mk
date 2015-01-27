841db583655922c6d34e69580f136d6aba712219
Randy DuCharme <radio.ad5gb@gmail.com>
2015-01-27 10:43:10 -0600

    Start over with original factory defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_OTA_ASSERT_DEVICE := SGH-I717,quincyatt
 
 
 # Kernel
-TARGET_KERNEL_CONFIG        := cyanogenmod_quincyatt_defconfig
+TARGET_KERNEL_CONFIG        := stock_sammy_defconfig
 TARGET_KERNEL_SOURCE        := kernel/samsung/msm8660-common
 ##TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-arm-eabi-4.7
 #TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7-12.10/bin/arm-linux-gnueabihf-
